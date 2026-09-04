13b6ef9531499a3e7abeaf71da11f0eefba5729e
pawpai <sspy2548@gmail.com>
2026-09-04 16:08:10 +0000

    Completed Lab 1: System Profiling

--- profiler.py
+++ profiler.py
@@ -5,6 +5,9 @@
 import os
 import platform
 import psutil
+print(f"OS Name: {platform.system()} {platform.release()}")
+print(f"Number of CPU Cores: {psutil.cpu_count(logical=True)}")
+print(f"Total RAM: {round(psutil.virtual_memory().total / (1024**3), 2)} GB")
 
 # TODO: Write your system profiler code here 
 # Follow the instructions in the Lab manual.
--- stress_test.py
+++ stress_test.py
@@ -3,6 +3,11 @@
 # Student ID: [Enter your ID here]
 # ==========================================
 import time
+import math
 
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+    math.factorial(50000)
+    
 # TODO: Write the intensive computation loop here
 # Follow the instructions in the Lab manual.
